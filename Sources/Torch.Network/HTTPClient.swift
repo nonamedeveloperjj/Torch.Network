d85e0a96071e0d340c41d0f95094c216610f0c94
nonamedeveloperjj <100957242+nonamedeveloperjj@users.noreply.github.com>
2023-04-05 17:25:25 +0300

    Add async/await support

--- Sources/Torch.Network/HTTPClient.swift
+++ Sources/Torch.Network/HTTPClient.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // sourcery: AutoMockable
 public protocol HTTPClientProtocol: AnyObject {
-    func sendRequest(requestComponents: RequestComponents, completion: @escaping (Result<Data?, Error>) -> Void)
+    func sendRequest(requestComponents: RequestComponents) async throws -> Result<Data?, Error>
 }
 
 public final class HTTPClient: HTTPClientProtocol {
@@ -27,35 +27,27 @@ public final class HTTPClient: HTTPClientProtocol {
         self.urlSession = urlSession
     }
     
-    public func sendRequest(requestComponents: RequestComponents, completion: @escaping (Result<Data?, Error>) -> Void) {
+    public func sendRequest(requestComponents: RequestComponents) async throws -> Result<Data?, Error> {
         let urlComponents = urlComponentsFactory.create(from: requestComponents)
         
         guard let url = urlComponents.url else {
-            completion(.failure(NetworkError.invalidURL))
-            return
+            return .failure(NetworkError.invalidURL)
         }
         
         let request = urlRequestFactory.create(from: requestComponents, url: url)
+        let (data, response) = try await urlSession.data(for: request, delegate: nil)
         
-        urlSession.createDataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse else {
-                completion(.failure(NetworkError.incorrectResponseType))
-                return
-            }
-            
-            let acceptableStatusCodes = 200...299
-            
-            switch response.statusCode {
-            case acceptableStatusCodes:
-                completion(.success(data))
-            default:
-                completion(.failure(NetworkError.unacceptableStatusCode))
-            }
-        }.resume()
+        guard let response = response as? HTTPURLResponse else {
+            return .failure(NetworkError.incorrectResponseType)
+        }
+        
+        let acceptableStatusCodes = 200...299
+        
+        switch response.statusCode {
+        case acceptableStatusCodes:
+            return .success(data)
+        default:
+            return .failure(NetworkError.unacceptableStatusCode)
+        }
     }
 }
